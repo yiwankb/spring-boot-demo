cfff553b2418715e08cb368e8795a5a63a4f4749
gaohaijiang <lutghj1990@126.com>
2020-03-01 20:18:26 +0800

    demo提交

---
+++ src/main/java/com/yiwankb/springbootdemo/controller/K8SController.java
@@ -0,0 +1,13 @@
+package com.yiwankb.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class K8SController {
+
+    @RequestMapping("/k8s")
+    public String k8s() {
+        return "hello K8s Allen";
+    }
+}
---
+++ src/test/java/com/yiwankb/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yiwankb.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
